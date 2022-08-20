24b71c1fc844ee69b2f7255b373c79990cc29e7a
ifzyy <johnsonnifemi8@gmail.com>
2022-08-20 01:51:41 +0100

    fixed linter

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import booksReducer, { getBook } from './books/books';
-import categoriesReducer from './Categories/Categories';
+import categoriesReducer from './categories/categories';
 
 const rootReducer = combineReducers({ booksReducer, categoriesReducer });
 const store = createStore(rootReducer, applyMiddleware(thunk));
