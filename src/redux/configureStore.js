c0b2ac19e4a363741ddc2d173f7e466b1e2b3cc9
ifzyy <johnsonnifemi8@gmail.com>
2022-08-20 01:10:19 +0100

    implemented api

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,10 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import bookReducer from './Book/Book';
-import categoriesReducer from './Categories/Categories';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import booksReducer, { getBook } from './books/books';
+import categoriesReducer from './categories/categories';
 
-const rootReducer = combineReducers({
-  books: bookReducer,
-  categories: categoriesReducer,
-});
-
-const store = configureStore({
-  reducer: rootReducer,
-});
+const rootReducer = combineReducers({ booksReducer, categoriesReducer });
+const store = createStore(rootReducer, applyMiddleware(thunk));
+store.dispatch(getBook());
 
 export default store;
